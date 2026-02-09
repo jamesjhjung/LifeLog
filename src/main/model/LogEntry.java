5fb82d07188b1a23d7f18a9f87b32ea35099897a
James Jung <jjungg@students.cs.ubc.ca>
2026-02-09 14:13:51 -0800

    Set up initial classes

---
+++ src/main/model/LogEntry.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDate;
+
+public class LogEntry {
+    private String title;
+    private String category;
+    private String description;
+    private int rating;
+    private LocalDate date;
+}
