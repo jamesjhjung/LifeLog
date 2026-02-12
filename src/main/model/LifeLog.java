c1524c44e381e4203525dace6d0bab2bd216d480
James Jung <jjungg@students.cs.ubc.ca>
2026-02-11 21:32:18 -0800

    Created methods and added their specifications

--- src/main/model/LifeLog.java
+++ src/main/model/LifeLog.java
@@ -1,7 +1,49 @@
 package model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class LifeLog {
     private ArrayList<LogEntry> entries;
+
+    // EFFECTS: construcs a LifeLog object
+    public LifeLog() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds given entry to log
+    public void addEntry(LogEntry entry) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes first entry with matching given title
+    //          and date, does nothing if no match is found
+    public void removeEntry(String title, LocalDate date) {
+        // stub
+    }
+
+    // EFFECTS: returns list of entries in added order
+    public ArrayList<LogEntry> getEntries() {
+        return null; // stub
+    }
+
+    // EFFECTS: returns list of entries added in
+    //          order of given category in log
+    public ArrayList<LogEntry> getEntriesByCategory(String category) {
+        return null; // stub
+    }
+
+    // EFFECTS: returns total hours spent in given
+    //          category, returns 0 if no match
+    public double getTotalHoursByCategory(String category) {
+        return 0; // stub
+    }
+
+    // EFFECTS: returns total hours spent across 
+    //          all entries
+    public double getTotalHours() {
+        return 0; // stub
+    }
 }
--- src/main/model/LogEntry.java
+++ src/main/model/LogEntry.java
@@ -6,6 +6,63 @@ public class LogEntry {
     private String title;
     private String category;
     private String description;
-    private int rating;
+    private double hours;
     private LocalDate date;
+
+    // REQUIRES: title and category are non-empty
+    //           hours >= 0
+    // EFFECTS: constructs a LogEntry object with given fields
+    public LogEntry(String title, String category, String description, double hours, LocalDate date) {
+        // stub
+    }
+
+    // EFFECTS: returns true if this.category matches given category
+    public boolean matchesCategory(String category) {
+        return false; // stub
+    }
+
+    // EFFECTS: returns string representation of this entry
+    public String toString() {
+        return null; // stub
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
